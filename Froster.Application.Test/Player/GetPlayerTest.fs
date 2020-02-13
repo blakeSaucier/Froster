22e42931ff8f112ad0e9c2f129f3f0fbe55acf5c
blakesaucier <blakesaucier@gmail.com>
2020-02-12 22:35:52 -0800

    add some unit tests

---
+++ Froster.Application.Test/Player/GetPlayerTest.fs
@@ -0,0 +1,29 @@
+﻿module Froster.Application.Test.Player.GetPlayerTest
+
+open NUnit.Framework
+open Froster.Domain
+open Froster.Application.GetPlayer
+open FsUnit
+
+let mockPlayer = 
+    {
+        Id = 1
+        FirstName = "James"
+        LastName = "Saucier"
+        Position = Some "Center"
+        PhoneNumber = "604503423"
+        Number = 89
+        Status = "FullTime"
+    }
+
+[<Test>]
+let ``Should Find Player`` () =
+    let fetchPlayer = fun (_: int) -> Some mockPlayer
+    let result = getPlayer fetchPlayer 1
+    result |> should equal (Some mockPlayer)
+
+[<Test>]
+let ``Should Not Find Player`` () =
+    let fetchPlayer = fun (_: int) -> None
+    let result = getPlayer fetchPlayer 1
+    result |> should equal None
