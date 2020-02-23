933c317520f2c742dfb09a7c8edaca64afdc906b
blakesaucier <blakesaucier@gmail.com>
2020-02-23 13:59:28 -0800

    Add db schema

---
+++ src/Infrastructure/Data/Schema/Migration.sql
@@ -0,0 +1,56 @@
+﻿Drop table [dbo].[Teams]
+GO
+
+CREATE TABLE [dbo].[Teams]
+(
+	[TeamId] INT NOT NULL PRIMARY KEY, 
+    [TeamName] NVARCHAR(50) NOT NULL, 
+    [Location] NVARCHAR(50) NULL, 
+    [TimeZone] NVARCHAR(50) NOT NULL
+)
+GO
+
+DROP TABLE [dbo].[Games]
+GO
+
+CREATE TABLE [dbo].[Games]
+(
+	[GameId] INT NOT NULL PRIMARY KEY,
+    [TeamId] INT FOREIGN KEY REFERENCES Teams(TeamId),
+    [StartTime] DATETIME NOT NULL,
+    [Location] NVARCHAR(50) NOT NULL,   
+    [Description] NVARCHAR(MAX) NULL
+)
+
+GO
+
+Drop table [dbo].[Players]
+GO
+
+CREATE TABLE [dbo].[Players] (
+    [PlayerId]     INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]    NVARCHAR (MAX) NOT NULL,
+    [LastName]     NVARCHAR (MAX) NOT NULL,
+    [Position]     NVARCHAR (MAX) NULL,
+    [JerseyNumber] INT            NOT NULL,
+    [PhoneNumber]  NVARCHAR (MAX) NOT NULL,
+    [Status]       NVARCHAR (MAX) NOT NULL,
+    [TeamId]       INT FOREIGN KEY REFERENCES Teams(TeamId)
+    CONSTRAINT [PK_Players] PRIMARY KEY CLUSTERED ([PlayerId] ASC)
+);
+
+GO
+
+DROP TABLE [dbo].[Attendances]
+GO
+
+CREATE TABLE [dbo].[Attendances]
+(
+    [AttendanceId]  INT NOT NULL PRIMARY KEY,
+    [GameId]        INT NOT NULL FOREIGN KEY REFERENCES Games(GameId),
+    [PlayerId]      INT NOT NULL FOREIGN KEY REFERENCES Players(PlayerId),
+    [Status]        INT NOT NULL,
+    [Note]          NVARCHAR(50) NULL
+);
+
+GO
